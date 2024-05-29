df4192e5ff9f8c4988ac62d76de92b1510915b7a
JangWoojun <woojun0107@naver.com>
2024-05-29 14:35:24 +0900

    feat: cropview 구현

--- app/src/main/java/com/woojun/androidcameracrop/MainActivity.kt
+++ app/src/main/java/com/woojun/androidcameracrop/MainActivity.kt
@@ -1,19 +1,10 @@
 package com.woojun.androidcameracrop
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.woojun.androidcameracrop.databinding.ActivityMainBinding
-import com.yalantis.ucrop.UCrop
-import java.io.File
-
-
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,40 +14,21 @@ class MainActivity : AppCompatActivity() {
     private var pickMedia = registerForActivityResult(
         ActivityResultContracts.PickVisualMedia()
     ) { uri ->
-        val destinationFileName = "SAMPLE_CROPPED_IMAGE_NAME.png"
-
-        val option = UCrop.Options().apply {
-            val pinkColor = ContextCompat.getColor(this@MainActivity, R.color.pink)
-            val whiteColor = ContextCompat.getColor(this@MainActivity, R.color.white)
-
-            setToolbarColor(pinkColor)
-            setStatusBarColor(pinkColor)
-            setToolbarWidgetColor(pinkColor)
-
-            setToolbarWidgetColor(whiteColor)
-        }
-
-        UCrop.of(uri!!, Uri.fromFile(File(cacheDir, destinationFileName)))
-            .withAspectRatio(1f, 1f)
-            .withMaxResultSize(400, 400)
-            .withOptions(option)
-            .start(this)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
-            val resultUri = UCrop.getOutput(data!!)
-            Toast.makeText(this, resultUri.toString(), Toast.LENGTH_SHORT).show()
-        } else if (resultCode == UCrop.RESULT_ERROR) {
-            val cropError = UCrop.getError(data!!)
-        }
+        binding.cropView.of(uri)
+            .withAspect(1, 1)
+            .withOutputSize(400, 400)
+            .initialize(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val croppedBitmap = binding.cropView.output
+            binding.imageView.setImageBitmap(croppedBitmap)
+        }
 
         pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
     }
