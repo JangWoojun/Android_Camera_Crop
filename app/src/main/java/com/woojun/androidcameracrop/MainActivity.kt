9fac5691d57bd1c0089d790e85dadd5d62eaa40f
JangWoojun <woojun0107@naver.com>
2024-05-29 14:23:02 +0900

    feat: uCrop 구현

--- app/src/main/java/com/woojun/androidcameracrop/MainActivity.kt
+++ app/src/main/java/com/woojun/androidcameracrop/MainActivity.kt
@@ -1,32 +1,56 @@
 package com.woojun.androidcameracrop
 
+import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.widget.Toast
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import com.canhub.cropper.CropImage
-import com.canhub.cropper.CropImageContract
-import com.canhub.cropper.CropImageContractOptions
-import com.canhub.cropper.CropImageOptions
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.woojun.androidcameracrop.databinding.ActivityMainBinding
+import com.yalantis.ucrop.UCrop
+import java.io.File
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    private val customCropImage = registerForActivityResult(CropImageContract()) {
-        if (it !is CropImage.CancelledResult) {
-            binding.imageView.setImageURI(it.uriContent)
-        }
-    }
 
     private var pickMedia = registerForActivityResult(
         ActivityResultContracts.PickVisualMedia()
     ) { uri ->
-        Handler().postDelayed({
-            startCameraWithoutUri(uri!!)
-        }, 500)
+        val destinationFileName = "SAMPLE_CROPPED_IMAGE_NAME.png"
+
+        val option = UCrop.Options().apply {
+            val pinkColor = ContextCompat.getColor(this@MainActivity, R.color.pink)
+            val whiteColor = ContextCompat.getColor(this@MainActivity, R.color.white)
+
+            setToolbarColor(pinkColor)
+            setStatusBarColor(pinkColor)
+            setToolbarWidgetColor(pinkColor)
+
+            setToolbarWidgetColor(whiteColor)
+        }
+
+        UCrop.of(uri!!, Uri.fromFile(File(cacheDir, destinationFileName)))
+            .withAspectRatio(1f, 1f)
+            .withMaxResultSize(400, 400)
+            .withOptions(option)
+            .start(this)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
+            val resultUri = UCrop.getOutput(data!!)
+            Toast.makeText(this, resultUri.toString(), Toast.LENGTH_SHORT).show()
+        } else if (resultCode == UCrop.RESULT_ERROR) {
+            val cropError = UCrop.getError(data!!)
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,16 +62,5 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun startCameraWithoutUri(uri: Uri) {
-        customCropImage.launch(
-            CropImageContractOptions(
-                uri = uri,
-                cropImageOptions = CropImageOptions(
-                    imageSourceIncludeCamera = false,
-                    imageSourceIncludeGallery = true,
-                ),
-            ),
-        )
-    }
 
 }
